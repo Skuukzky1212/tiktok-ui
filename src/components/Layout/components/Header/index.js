5808a6fd53b5d3651566b7497b09cda8e0087c3d
Pham Quoc Vuong <Vuongnguyen30702@gmail.com>
2022-05-31 15:26:04 +0700

    Header-ui-commit3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,19 +1,42 @@
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+    faCircleQuestion,
     faCircleXmark,
+    faCloudUploadAlt,
+    faEarthAsia,
+    faEllipsisVertical,
+    faKeyboard,
     faMagnifyingGlass,
+    faSignIn,
     faSpinner,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts'
+    },
+];
 
 function Header() {
     const [searchRessult, setSearchRessult] = useState([]);
@@ -68,7 +91,26 @@ function Header() {
                         </div>
                     </Tippy>
                 </div>
-                <div className={cx('action')}></div>
+                <div className={cx('action')}>
+                    <Button
+                        text
+                        leftIcon={<FontAwesomeIcon icon={faCloudUploadAlt} />}
+                    >
+                        Upload
+                    </Button>
+                    <Button
+                        primary
+                        leftIcon={<FontAwesomeIcon icon={faSignIn} />}
+                    >
+                        Log in
+                    </Button>
+
+                    <Menu items={ MENU_ITEMS }>
+                        <div className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </div>
+                    </Menu>
+                </div>
             </div>
         </header>
     );
