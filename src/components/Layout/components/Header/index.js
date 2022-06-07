d4debc30b60488d94c237dc82bc59d274e568a35
Pham Quoc Vuong <Vuongnguyen30702@gmail.com>
2022-06-07 17:03:33 +0700

    Header-ui-commit4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -10,10 +10,17 @@ import {
     faMagnifyingGlass,
     faSignIn,
     faSpinner,
+    faCloudUpload,
+    faPaperPlane,
+    faUser,
+    faCoins,
+    faGear,
+    faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
+import HeadlessTippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 import classNames from 'classnames/bind';
-
 import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
@@ -22,30 +29,84 @@ import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+
 const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
-        title: 'English'
+        title: 'English',
+        children: {
+            title: 'language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
         title: 'Feedback and help',
-        to: '/feedback'
+        to: '/feedback',
     },
     {
         icon: <FontAwesomeIcon icon={faKeyboard} />,
-        title: 'Keyboard shortcuts'
+        title: 'Keyboard shortcuts',
     },
 ];
 
 function Header() {
     const [searchRessult, setSearchRessult] = useState([]);
 
+    const currentUser = true;
+
     useEffect(() => {
         setTimeout(() => {
             setSearchRessult([]);
         }, 0);
     }, []);
+
+    // handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                //handle change language
+                break;
+            default:
+        }
+    };
+
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile ',
+            to: '/@Vuong',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get coins',
+            to: '/coins',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignOut} />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -53,7 +114,7 @@ function Header() {
                     <img src={images.logo} alt="tiktok-logo" />
                 </div>
                 <div>
-                    <Tippy
+                    <HeadlessTippy
                         interactive={true}
                         visible={searchRessult.length > 0}
                         render={(attrs) => (
@@ -89,26 +150,64 @@ function Header() {
                                 <FontAwesomeIcon icon={faMagnifyingGlass} />
                             </button>
                         </div>
-                    </Tippy>
+                    </HeadlessTippy>
                 </div>
                 <div className={cx('action')}>
-                    <Button
-                        text
-                        leftIcon={<FontAwesomeIcon icon={faCloudUploadAlt} />}
-                    >
-                        Upload
-                    </Button>
-                    <Button
-                        primary
-                        leftIcon={<FontAwesomeIcon icon={faSignIn} />}
-                    >
-                        Log in
-                    </Button>
+                    {currentUser ? (
+                        <>
+                            <Tippy
+                                content="Upload Video"
+                                placement="bottom"
+                                delay={[0, 100]}
+                            >
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faCloudUpload} />
+                                </button>
+                            </Tippy>
 
-                    <Menu items={ MENU_ITEMS }>
-                        <div className={cx('more-btn')}>
-                            <FontAwesomeIcon icon={faEllipsisVertical} />
-                        </div>
+                            <Tippy
+                                content="Message"
+                                placement="bottom"
+                                delay={[0, 100]}
+                            >
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faPaperPlane} />
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button
+                                text
+                                leftIcon={
+                                    <FontAwesomeIcon icon={faCloudUploadAlt} />
+                                }
+                            >
+                                Upload
+                            </Button>
+                            <Button
+                                primary
+                                leftIcon={<FontAwesomeIcon icon={faSignIn} />}
+                            >
+                                Log in
+                            </Button>
+                        </>
+                    )}
+                    <Menu
+                        items={currentUser ? userMenu : MENU_ITEMS}
+                        onChange={handleMenuChange}
+                    >
+                        {currentUser ? (
+                            <img
+                                className={cx('user-avatar')}
+                                src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/283042525_3127793060792907_687868091453720612_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=v3eL5mA8QjoAX-He9uq&_nc_oc=AQllL028L4HM7S82f_al_FQGTmXeDN3UTPA0W9Y09dCdDvH4-lG2ndLcdXOe2ir41X4&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT8SM7TRw_hhR8_AsnKPgWMDks_ho9HfrJdtA8MvyKimEg&oe=629E0180"
+                                alt="Pham Quoc Vuong"
+                            />
+                        ) : (
+                            <div className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </div>
+                        )}
                     </Menu>
                 </div>
             </div>
